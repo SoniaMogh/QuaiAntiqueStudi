2c9838e1b80b56f78c536158dea2a3933e7d56da
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-04-20 23:33:15 +0200

    Routage system initialized

---
+++ Router/allRoutes.js
@@ -0,0 +1,7 @@
+import Route from "./route.js";
+
+//Définir ici vos routes
+export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
